2f9763d772849d27f227608ba2063e8e324daee4
Telmo Sampaio <telmoasampaio@gmail.com>
2020-10-26 23:37:34 +0000

    Tests passing for rendering multiple li inside ol

--- src/Appointment.js
+++ src/Appointment.js
@@ -4,5 +4,18 @@ export const Appointment = ({ customer: { firstName } }) => (
   <div>{firstName}</div>
 );
 
-export const AppointmentsDayView = () => 
-  <div id="appointmentsDayView"></div>;
+const appointmentTimeOfDay = startsAt => {
+  const [h, m] = new Date(startsAt).toTimeString().split(':');
+  return `${h}:${m}`;
+}
+
+export const AppointmentsDayView = ({ appointments }) => 
+  <div id="appointmentsDayView">
+    <ol>
+      {appointments.map((appointment ) => (
+        <li key={appointment.startsAt}>
+          {appointmentTimeOfDay(appointment.startsAt)}
+        </li>
+      ))}
+    </ol>
+  </div>;
