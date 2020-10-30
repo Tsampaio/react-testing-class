d29fe2e1546c043f4d3fe72fe78c8111da272fbd
Telmo Sampaio <telmoasampaio@gmail.com>
2020-10-30 23:42:40 +0000

    Testing if form exists

--- test/CustomerForm.test.js
+++ test/CustomerForm.test.js
@@ -9,10 +9,28 @@ describe('CustomerForm', () => {
     ({ render, container } = createContainer());
   });
 
+  const form = id => container.querySelector(`form[id="${id}"]`);
+
+  const expectToBeInputFieldOfTypeText = formElement => {
+    expect(formElement).not.toBeNull();
+    expect(formElement.tagName).toEqual('INPUT');
+    expect(formElement.type).toEqual('text');
+  };
+
   it('renders a form', () => {
     render(<CustomerForm />);
-    expect(
-      container.querySelector('form[id="customer"]')
-    ).not.toBeNull();
+    expect(form('customer')).not.toBeNull();
+  });
+
+  it('renders the first name field as a text box', () => {
+    render(<CustomerForm />);
+    const field = form('customer').elements.firstName;
+    expectToBeInputFieldOfTypeText( field );
+  });
+
+  it('includes the existing value for the first name', () => {
+    render(<CustomerForm firstName="Ashley" />);
+    const field = form('customer').elements.firstName;
+    expect(field.value).toEqual('Ashley');
   });
 });
